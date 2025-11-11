1b38d826f18e637b36a45e92e1d12c6cd97f0c0b
sirishabuddharaju <sirishabuddharaju2003@gmail.com>
2025-11-11 15:18:42 +0530

    "Commitng 3 change"

--- GitPractise/src/main/java/nit/GitPractise/App.java
+++ GitPractise/src/main/java/nit/GitPractise/App.java
@@ -2,7 +2,7 @@ package nit.GitPractise;
 
 public class App {
     public static void main(String[] args) {
-    	System.out.println("Hi EveryOne");
+    	System.out.println("Hi EveryOne this is me");
         System.out.println("Hello World! My Self Sirisha");
         System.out.println("Tl CHanged The code");
         System.out.println("TL Window");
