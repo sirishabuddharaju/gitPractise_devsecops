f04c91aea98f65a0cac99f0243c4e534b184d833
sirishabuddharaju <sirishabuddharaju2003@gmail.com>
2025-11-11 14:37:01 +0530

    "My First Commit "

---
+++ GitPractise/src/main/java/nit/GitPractise/App.java
@@ -0,0 +1,10 @@
+package nit.GitPractise;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! My Self Sirisha");
+    }
+}
