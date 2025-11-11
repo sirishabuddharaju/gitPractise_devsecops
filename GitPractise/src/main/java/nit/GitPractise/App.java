8b3350558f02f5a0c0d6e365e3c44544eeadc032
sirishabuddharaju <sirishabuddharaju2003@gmail.com>
2025-11-11 14:50:18 +0530

    "Devloper commit his project "

--- GitPractise/src/main/java/nit/GitPractise/App.java
+++ GitPractise/src/main/java/nit/GitPractise/App.java
@@ -6,5 +6,6 @@ package nit.GitPractise;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! My Self Sirisha");
+        System.out.println(" Hi I am Doing My Performence well over here");
     }
 }
