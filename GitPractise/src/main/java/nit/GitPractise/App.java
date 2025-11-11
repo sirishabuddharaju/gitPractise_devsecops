ccc25724c80bd24f61ec8159c6b6195be0923ec8
sirishabuddharaju <sirishabuddharaju2003@gmail.com>
2025-11-11 15:00:43 +0530

    " Tl Second commit"

--- GitPractise/src/main/java/nit/GitPractise/App.java
+++ GitPractise/src/main/java/nit/GitPractise/App.java
@@ -6,6 +6,7 @@ package nit.GitPractise;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! My Self Sirisha");
+        System.out.println("Tl CHanged The code");
         System.out.println(" Hi I am Doing My Performence well over here");
     }
 }
