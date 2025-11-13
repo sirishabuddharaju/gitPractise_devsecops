e1dcd8452f37e10534eb27dfcb78b8155c871e74
sirishabuddharaju <sirishabuddharaju2003@gmail.com>
2025-11-13 11:56:25 +0530

    "stash s1 changes"

--- GitPractise/src/main/java/nit/GitPractise/b1.java
+++ GitPractise/src/main/java/nit/GitPractise/b1.java
@@ -3,5 +3,7 @@ package nit.GitPractise;
 public class b1 {
 	 public static void main(String[] args) {
  System.out.println("Branch1 Is Created with name b1 and coded added by dev1");
+ 
+ System.out.println(" creating stash");
 }
 }
